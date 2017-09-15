fb48d7c1774c9525250d5e42922d6ee61b832af6
Ilya Ashikhmin <ashikhmin.ilya@gmail.com>
2017-09-15 11:10:45 +0300

    HTTP Service implemented.

---
+++ src/main/java/com/digdes/school/app/service/impl/HttpSerivceImpl.java
@@ -0,0 +1,37 @@
+package com.digdes.school.app.service.impl;
+
+import com.digdes.school.app.service.HttpService;
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+/**
+ * ...
+ *
+ * @author Ilya Ashikhmin (ashikhmin.ilya@gmail.com)
+ */
+@Service
+public class HttpSerivceImpl implements HttpService {
+    @Override
+    public String getUrl(String link) {
+        try {
+            URL url = new URL(link);
+            try (InputStream is = url.openStream()) {
+                BufferedReader br = new BufferedReader(new InputStreamReader(is));
+                StringBuilder sb = new StringBuilder();
+                String line;
+                while ((line = br.readLine()) != null) {
+                    sb.append(line);
+                }
+                return sb.toString();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
